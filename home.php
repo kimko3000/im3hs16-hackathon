ea1a535ef93ef2e1812c470c45d4259cc518e21b
Luca Toneatti <lucatoneatti@hotmail.com>
2016-11-10 17:11:13 +0100

    user_id Angepasst

--- home.php
+++ home.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-  if (!isset($_SESSION['id'])) {
+  if (!isset($_SESSION['user_id'])) {
       header('Location:login.php');
   } else {
-      $user_id = $_SESSION['id'];
+      $user_id = $_SESSION['user_id'];
   }
 
 
