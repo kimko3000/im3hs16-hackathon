6ca2038e5e6ea49739596d7038450b944b382474
Kim Schläpfer <kimschlaepfer@gmx.ch>
2016-11-10 11:12:50 +0100

    index up and ready

---
+++ index.php
@@ -0,0 +1,30 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="author" content="Kim Schläpfer, Luca Toneatti, Fabio Follador">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+  </head>
+  <body>
+
+
+
+
+
+
+
+
+
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/tether.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
