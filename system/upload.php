9899b9f50f223ef46cf82aeb3491f5356e1a8f8b
Luca Toneatti <lucatoneatti@hotmail.com>
2016-11-28 14:38:31 +0100

    Code in System eigefügt

--- system/upload.php
+++ system/upload.php
@@ -1,62 +1,60 @@
 <?php
 
-  /*
-  * Create a random string
-  * @author	XEWeb <>
-  * @param $length the length of the string to create
-  * @return $str the string
-  * https://www.xeweb.net/2011/02/11/generate-a-random-string-a-z-0-9-in-php/
-  */
-  function randomString($length = 8) {
-    $str = "";
-    $characters = array_merge(range('A','Z'), range('a','z'), range('0','9'));
-    $max = count($characters) - 1;
-    for ($i = 0; $i < $length; $i++) {
-    	$rand = mt_rand(0, $max);
-    	$str .= $characters[$rand];
+
+  // für Spätere Verwendung initialisieren wir die Variablen $error, $error_msg, $success, $success_msg
+  $error = false;
+  $error_msg = "";
+  $success = false;
+  $success_msg = "";
+
+  if(isset($_POST['upload-submit'])){
+    if(!empty($_POST['title']) && !empty($_POST['description']) && !empty($_POST['alt']) && !empty($_POST['long'])){
+      $title = filter_data($_POST['title']);
+      $description = filter_data($_POST['description']);
+      $alt = filter_data($_POST['alt']);
+      $long = filter_data($_POST['long']);
+      $file_name = "";
+      // Bildupload
+      $uploadOk = true;
+      $upload_path = "img/";   // Zielverzeichnis für hochzuladene Datei
+      $max_file_size = 500000;      // max. Dateigrösse in Byte
+
+      // Filetype kontrollieren
+      if ( ($_FILES['img']['name']  != "")){
+          $filetype = $_FILES['img']['type'];
+          switch($filetype){
+              case "image/jpeg":
+                  $file_extension = "jpg";
+                  break;
+              case "image/gif":
+                  $file_extension = "gif";
+                  break;
+              case "image/png":
+                  $file_extension = "png";
+                  break;
+              default:
+                $uploadOk = false;
+          }
+
+          // Dateigrösse kontrollieren
+          $upload_filesize = $_FILES["img"]["size"];
+          if ( $upload_filesize >= $max_file_size || $_FILES["img"]["error"]) {
+              echo "Leider ist die Datei mit $upload_filesize KB zu gross. <br> Sie darf nicht grösser als $max_file_size sein. ";
+              $uploadOk = false;
+          }
+
+          if (!$uploadOk) {
+              echo "Leider konnte die Datei nicht hochgeladen werden.";
+          } else {
+              $file_name = time() . "." . $file_extension;
+              move_uploaded_file($_FILES['img']['tmp_name'], $upload_path . $file_name );
+              $result = upload($title, $description, $alt, $long, $file_name);
+          }
+      }
     }
-    return $str;
-  }
-
-
-
-  // Bildupload
-  function upload_image($image_file, $upload_path, $max_file_size = 500000){
-
-    $uploadOk = true;
-
-    // Filetype kontrollieren
-  	if ( ($image_file['name']  != "")){
-  		$filetype = $image_file['type'];
-  		switch($filetype){
-  			case "image/jpeg":
-  				$file_extension = "jpg";
-  				break;
-  			case "image/gif":
-  				$file_extension = "gif";
-  				break;
-  			case "image/png":
-  				$file_extension = "png";
-  				break;
-  		}
-  		// Dateigrösse kontrollieren
-  		$upload_filesize = $image_file["size"];
-      if ( $upload_filesize > $max_file_size) {
-        echo "Leider ist die Datei mit $upload_filesize KB zu gross. <br> Sie darf nicht grösser als $max_file_size sein. <br>";
-        $uploadOk = false;
-      }
-
-      if ($uploadOk)
-      {
-        $file_name = time() . $str . "." . $file_extension;
-        move_uploaded_file( $image_file['tmp_name'] , $upload_path . $file_name );
-  	  } else {
-        echo "Leider konnte die Datei nicht hochgeladen werden.";
-        $file_name = "default";
-      }
-  	}
-
-  	return $file_name;
-
+    else{
+      $error = true;
+      $error_msg .= "Bitte füllen Sie alle Felder aus.</br>";
+    }
   }
 ?>
--- upload.php
+++ upload.php
@@ -12,62 +12,7 @@ require_once("system/data.php");
 require_once("system/security.php");
 require_once("system/upload.php");
 
-// für Spätere Verwendung initialisieren wir die Variablen $error, $error_msg, $success, $success_msg
-$error = false;
-$error_msg = "";
-$success = false;
-$success_msg = "";
 
-if(isset($_POST['upload-submit'])){
-  if(!empty($_POST['title']) && !empty($_POST['description']) && !empty($_POST['alt']) && !empty($_POST['long'])){
-    $title = filter_data($_POST['title']);
-    $description = filter_data($_POST['description']);
-    $alt = filter_data($_POST['alt']);
-    $long = filter_data($_POST['long']);
-    $file_name = "";
-    // Bildupload
-    $uploadOk = true;
-    $upload_path = "img/";   // Zielverzeichnis für hochzuladene Datei
-    $max_file_size = 500000;      // max. Dateigrösse in Byte
-
-    // Filetype kontrollieren
-    if ( ($_FILES['img']['name']  != "")){
-        $filetype = $_FILES['img']['type'];
-        switch($filetype){
-            case "image/jpeg":
-                $file_extension = "jpg";
-                break;
-            case "image/gif":
-                $file_extension = "gif";
-                break;
-            case "image/png":
-                $file_extension = "png";
-                break;
-            default:
-              $uploadOk = false;
-        }
-
-        // Dateigrösse kontrollieren
-        $upload_filesize = $_FILES["img"]["size"];
-        if ( $upload_filesize >= $max_file_size || $_FILES["img"]["error"]) {
-            echo "Leider ist die Datei mit $upload_filesize KB zu gross. <br> Sie darf nicht grösser als $max_file_size sein. ";
-            $uploadOk = false;
-        }
-
-        if (!$uploadOk) {
-            echo "Leider konnte die Datei nicht hochgeladen werden.";
-        } else {
-            $file_name = time() . "." . $file_extension;
-            move_uploaded_file($_FILES['img']['tmp_name'], $upload_path . $file_name );
-            $result = upload($title, $description, $alt, $long, $file_name);
-        }
-    }
-  }
-  else{
-    $error = true;
-    $error_msg .= "Bitte füllen Sie alle Felder aus.</br>";
-  }
-}
 
 
 
