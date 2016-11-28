0f669ed4d537357fdbdf7e536c11fa4d61bb338d
Kim Schläpfer <kimschlaepfer@gmx.ch>
2016-11-28 13:02:14 +0100

    upload
    upload reihenfolge

--- system/data.php
+++ system/data.php
@@ -43,8 +43,8 @@ function checkforexistance($username, $email){
 /* Fotos
 /* ****************************************************** */
 
-function upload($title, $description, $file_name, $alt, $long){
-  $sql = "INSERT INTO picture (`title`, `description`, `file_name`, `alt`, `long`) VALUES ('$title', '$description', '$file_name', '$alt', '$long');";
+function upload($title, $description, $alt, $long, $file_name){
+  $sql = "INSERT INTO picture (`title`, `description`, `alt`, `long`, `file_name`) VALUES ('$title', '$description', '$alt', '$long', '$file_name');";
   return get_result($sql);
 }
 
